841fd8aca5b923c881dec027dc7e6036c370bad3
Unknown <info@cloudmill.ru>
2018-06-25 15:10:20 +0300

    up

--- scripts/main.js
+++ scripts/main.js
@@ -350,7 +350,13 @@ $(document).ready(function () {
             gallery: {
                 enabled: true,
                 navigateByImgClick: true,
-                preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+                preload: [0, 1]
+            },
+            image: {
+                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+                titleSrc: function titleSrc(item) {
+                    return item.el.attr('title');
+                }
             }
         });
     });
